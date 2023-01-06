c79bfa20cdbd9738b609dc1b017df6a19a79f926
ravi nandan <monubibhu@gmail.com>
2023-01-07 00:41:50 +0530

    first commit

---
+++ enroll/views.py
@@ -0,0 +1,38 @@
+from email.policy import HTTP
+from django.shortcuts import render, HttpResponseRedirect
+from .forms import StudentRegistration
+from .models import User
+# Create your views here.
+
+# This function adds new item and show to screen
+def add_show(request):
+    if request.method == 'POST':
+        fm = StudentRegistration(request.POST)
+        if fm.is_valid():
+            fm.save()
+            fm = StudentRegistration()
+    else:
+        fm = StudentRegistration()
+    stud = User.objects.all()
+    return render(request, 'enroll/addandshow.html', {'form':fm, 'stu':stud})
+
+
+
+#This function update/edit data
+def update_data(request, id):
+    if request.method == 'POST':
+        pi = User.objects.get(pk=id)
+        fm = StudentRegistration(request.POST, instance=pi)
+        if fm.is_valid():
+            fm.save()
+    else:
+        pi = User.objects.get(pk=id)
+        fm = StudentRegistration(instance=pi)
+    return render(request, 'enroll/updatestudent.html', {'form':fm})
+
+# This function will delete data from database from screen
+def delete_data(request, id):
+    if request.method == 'POST':
+        pi = User.objects.get(pk=id)
+        pi.delete()
+        return HttpResponseRedirect('/')
